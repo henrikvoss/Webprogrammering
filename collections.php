70b3d06017ccc8da143ac29286ae9aee3a0717ad
Johan S. Steinberg <s171172@stud.hio.no>
2011-04-15 15:46:18 +0200

    Reparerte register.php: flyttet sessionDatabase inn i if'fen.

--- collections.php
+++ collections.php
@@ -22,17 +22,8 @@
 
 	<section id="collectionsHeight">
 		<nav id="collections">
-			<a href="2011SS1.php" class="ss2011">
-				SS2011 collection.<span></span>
-			</a>
-			<a href="2010AW1.php" class="aw2010">
-				AW2010 collection.<span></span>
-			</a>
 			<a href="2010SS1.php" class="ss2010">
 				SS2010 collection.<span></span>
-			</a>
-			<a href="2009SS1.php" class="ss2009">
-				SS2009 collection.<span></span>
 			</a>
 		</nav>
 	</section>
--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 <!DOCTYPE HTML>
 
 <html lang="en">
-	
+
 	<head>
 		<meta charset="UTF-8" />
 
@@ -11,75 +11,74 @@
 		<meta name="keywords" content="contact, Vatle,
 		clothing, women&#39;s clothing, soon men&#39;s clothing, designer clothing, Vatle
 		designs" />
-		
+
 		<title>VATLE - Register at Vatle's webshop</title>
-		
+
 		<?php addLinkTags(); ?>
 	</head>
-	
+
 	<body>
 		<?php printHeader(); ?>
-		
+
 		<section class="text">
-		
+
 		<h1>Register with VATLEdesign</h1>
-		
-		<?php  
-		
-		if(isset($_REQUEST["Register"]))
-		{
-			$_SESSION['Database'] =
-				new Database('cube.iu.hio.no', 's171172', '', 's171172');
-		}
-		
-		else
-		{
-		?>
-		
+
+<?php  
+
+if(isset($_REQUEST["Register"]))
+{
+	$_SESSION['Database'] =
+		new Database('cube.iu.hio.no', 's171172', '', 's171172');
+	$_SESSION['Database']->addData($_POST['email'],$_POST['password'], $_POST['first'], $_POST['surname'], $_POST['address'], $_POST['postalcode'], $_POST['city'], $_POST['country']);		
+}
+else
+{
+?>
+
 		<form id="register" action="" method="post">
 			<table border="0" >
-		      <tr>
-		        <td>First name:</td>
-		        <td><input type="text" name="first"></td>
-		      </tr>
-		      <tr>
-		        <td>Surname:</td>
-		        <td><input type="text" name="surname"></td>
-		      </tr>
 				<tr>
-		        <td>Address:</td>
-		        <td><input type="text" name="address"></td>
-		      </tr>
+				  <td>First name:</td>
+				  <td><input type="text" name="first"></td>
+				</tr>
 				<tr>
-		        <td>City/State:</td>
-		        <td><input type="text" name="city"></td>
-		      </tr>
+				  <td>Surname:</td>
+				  <td><input type="text" name="surname"></td>
+				</tr>
 				<tr>
-		        <td>Zip code:</td>
-		        <td><input type="text" name="postalcode"></td>
-		      </tr>
+				  <td>Address:</td>
+				  <td><input type="text" name="address"></td>
+				</tr>
 				<tr>
-		        <td>Country:</td>
-		        <td><input type="text" name="country"></td>
-		      </tr>
-		      <tr>
-		        <td>E-mail:</td>
-		        <td><input type="text" name="email"></td>
-		      </tr>
+				  <td>City/State:</td>
+				  <td><input type="text" name="city"></td>
+				</tr>
 				<tr>
-		        <td>Password:</td>
-		        <td><input type="password" name="password"></td>
-		      </tr>				
-		      <tr><td><input type="submit" name="send" VALUE="Register"></td></tr>
-		     </table>
-			
+				  <td>Zip code:</td>
+				  <td><input type="text" name="postalcode"></td>
+				</tr>
+				<tr>
+				  <td>Country:</td>
+				  <td><input type="text" name="country"></td>
+				</tr>
+				<tr>
+				  <td>E-mail:</td>
+				  <td><input type="text" name="email"></td>
+				</tr>
+				<tr>
+				  <td>Password:</td>
+				  <td><input type="password" name="password"></td>
+				</tr>				
+				<tr><td><input type="submit" name="send" VALUE="Register"></td></tr>
+			  </table>
+
 		</form>
-		<?php 
-			}
-			$_SESSION['Database']->addData($_POST['email'],$_POST['password'], $_POST['first'], $_POST['surname'], $_POST['address'], $_POST['postalcode'], $_POST['city'], $_POST['country']);		
-		?>
+<?php 
+}
+?>
 		</section>
-		
+
 		<?php printFooter(); ?>
 	</body>
 
