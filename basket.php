5f753699ff61d50663590ae3c32596200ccd5770
Johan S. Steinberg <johansst@gmail.com>
2011-05-12 17:23:40 +0200

    Litt på basket.php.

---
+++ basket.php
@@ -0,0 +1,21 @@
+<?php include("controller.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8" />
+
+	<title>
+		VATLE - Webshop - Basket
+	</title>
+
+	<?php addLinkTags(); ?>
+</head>
+
+<body>
+	<?php printHeader(); ?>
+	<?php printFooter(); ?>
+</body>
+
+</html>
+
