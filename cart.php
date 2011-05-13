67d2e19d87719bb5b32ddbacd22f2d763d1766cd
Johan S. Steinberg <johansst@gmail.com>
2011-05-13 16:32:47 +0200

    Forsøk på å slette vare fra handlekurv.

--- cart.php
+++ cart.php
@@ -1,38 +1,22 @@
 <?php
 include("controller.php");
 
-$itemInStock = array();
-
-if ( isset($_REQUEST["addToCart"]) ) {
-
-	if ( !isset($_SESSION["cart"]) ) {
-		$_SESSION["cart"] = new Cart();
-	}
-
-	for ( $i = 0; $i < count($_SESSION["style"]); $i++ ) {
-		if (isset($_REQUEST[("amount".$i)])){
-			if ( $_REQUEST[("amount".$i)] != 0) {
-				$itemInStock[$i] = $_SESSION["cart"]->addToCart($i,$_REQUEST[("amount".$i)]);
-			}
-		}
-	}
-}
-
 function printCart($cart) {
 	foreach ($cart as $key=>$style) {?>
-		<div class="browseStyles">
+	<div class="browseStyles">
 
-			<img class="floatLeft" src="<?php echo $style->getImage(); ?>"
-			alt="<?php echo $style->getName(); ?>"/>
-			<p>Style: <?php echo $style->getName(); ?></p>
-			<p>Price: <?php echo $style->getPrice(); ?></p>
-			<p>You have <?php echo $style->getAmountInCart(); ?> in your cart.</p>
-			<!-- En form for å slette og endre antall. -->
-			<form action="cart.php" method="post">
-				<input type="text" name="newAmount" value="<?php echo $style->getAmountInCart(); ?>" />
-				<input type="submit" name="updateItem" value="Change amount" />
-				<input type="submit" name="deleteItem" value="Delete this item" />
-			</form>
+		<img class="floatLeft" src="<?php echo $style->getImage(); ?>"
+		alt="<?php echo $style->getName(); ?>"/>
+		<p>Style: <?php echo $style->getName(); ?></p>
+		<p>Price: <?php echo $style->getPrice(); ?></p>
+		<p>You have <?php echo $style->getAmountInCart(); ?> in your cart.</p>
+		<!-- En form for å slette og endre antall. -->
+		<form action="cart.php" method="post">
+			<input type="hidden" name="cartKey" value="<?php echo $key ?>" />
+			<input type="text" name="newAmount" value="<?php echo $style->getAmountInCart(); ?>" />
+			<input type="submit" name="updateItem" value="Change amount" />
+			<input type="submit" name="deleteItem" value="Delete this item" />
+		</form>
 
 		</div><?php
 	}
@@ -56,24 +40,49 @@ function printCart($cart) {
 <body>
 	<?php printHeader(); ?>
 	<section class="text">
-		<h1>Your cart</h1>
+
+<?php
+if (!isset($_SESSION["user"])) {
+	?><a href="index.php">Go to shop</a><?php
+} else {
+?>
+
+<h1>Your cart</h1>
 
 <?php
 
-if ( isset($_REQUEST["addToCart"]) ) {
-	foreach ($itemInStock as $key=>$value) {
-		if ( !$value ) {	
-			?><p>Item <?php echo $_SESSION["style"][$key]->getName(); ?> is out of stock.</p><?php
-		} else {
-			?><p><?php echo $value." of item ".$_SESSION["style"][$key]->getName(); ?> is available.</p><?php
+	$itemInStock = array();
+
+	if ( isset($_REQUEST["addToCart"]) ) {
+
+		if ( !isset($_SESSION["cart"]) ) {
+			$_SESSION["cart"] = new Cart();
+		}
+
+		for ( $i = 0; $i < count($_SESSION["style"]); $i++ ) {
+			if (isset($_REQUEST[("amount".$i)])){
+				if ( $_REQUEST[("amount".$i)] != 0) {
+					$itemInStock[$i] = $_SESSION["cart"]->addToCart($i,$_REQUEST[("amount".$i)]);
+				}
+			}
+		}
+
+		foreach ($itemInStock as $key=>$value) {
+			if ( !$value ) {	
+				?><p>Item <?php echo $_SESSION["style"][$key]->getName(); ?> is out of stock.</p><?php
+			} else {
+				?><p><?php echo $value." of item ".$_SESSION["style"][$key]->getName(); ?> is available.</p><?php
+			}
 		}
 	}
-	?></p><?php
+
+	if ( isset($_REQUEST["deleteItem"]) ) {
+		$_SESSION["cart"]->deleteItem($_REQUEST["cartKey"]);
+	}
+
+	printCart($_SESSION["cart"]->getCart());
 }
-
 ?>
-
-<?php printCart($_SESSION["cart"]->getCart()); ?>
 
 	</section>
 	<?php printFooter(); ?>
